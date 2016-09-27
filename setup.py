1a44bac8134241a8f6eed918f4e16c91170b0351
Aaron LI <aaronly.me@outlook.com>
2016-09-27 13:11:07 +0800

    setup.py: add classifier and requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python3
+#
+# References:
+# [1] Python Packaging User Guide
+#     https://packaging.python.org/
+#
+
+import os
 
 from setuptools import setup, find_packages
 
 import fg21sim as pkg
 
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(
     name=pkg.__pkgname__,
     version=pkg.__version__,
     description=pkg.__description__,
-    long_description=open("README.rst").read(),
+    long_description=read("README.rst"),
     author=pkg.__author__,
     author_email=pkg.__author_email__,
     url=pkg.__url__,
     license=pkg.__license__,
-    packages=find_packages(exclude=("tests", "docs")),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    packages=find_packages(exclude=["docs", "tests"]),
     scripts=[
         "bin/healpix2hpx",
         "bin/hpx2healpix",
     ],
+    install_requires=[
+        "numpy",
+        "astropy",
+        "healpy",
+    ],
+    tests_require=["pytest"],
 )
