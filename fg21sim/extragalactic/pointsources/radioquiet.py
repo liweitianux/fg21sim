5c64ae7b8e3985f61e733fc40fbba32c428d79a6
Zhixian Ma <zxma_sjtu@qq.com>
2016-10-19 23:26:21 +0800

    Add new methods to calculate Tb.

--- fg21sim/extragalactic/pointsources/radioquiet.py
+++ fg21sim/extragalactic/pointsources/radioquiet.py
@@ -3,48 +3,26 @@
 
 import numpy as np
 import healpy as hp
+import astropy.units as au
 
-from .flux import Flux
 from .base import BasePointSource
+from ...utils import convert
+
 
 class RadioQuiet(BasePointSource):
-    def __init__(self,configs):
+
+    def __init__(self, configs):
         super().__init__(configs)
-        self._get_configs()
+        self._set_configs()
 
-    def _get_configs(self):
+    def _set_configs(self):
         """Load the configs and set the corresponding class attributes"""
-
+        super()._set_configs()
         # point sources amount
         self.num_ps = self.configs.getn("extragalactic/pointsources/num_rq")
         # prefix
         self.prefix = self.configs.getn(
             "extragalactic/pointsources/prefix_rq")
-
-    def calc_flux(self, freq):
-        """
-        Calculate the flux and surface brightness of the point source.
-
-        Parameters
-        ----------
-        ps_type: int
-            Type of point source
-        freq: float
-            frequency
-        ps_frame: pandas.core.frame.DataFrame
-            Data of the point sources
-        """
-        # init flux
-        ps_flux = Flux(freq, 3)
-        # ps_flux_list
-        num_ps = self.ps_catelog.shape[0]
-        ps_flux_list = np.zeros((num_ps,))
-        # Iteratively calculate flux
-        for i in range(num_ps):
-            ps_area = self.ps_catelog['Area (sr)'][i]
-            ps_flux_list[i] = ps_flux.calc_Tb(ps_area)
-
-        return ps_flux_list
 
     def draw_single_ps(self, freq):
         """
@@ -54,7 +32,7 @@ class RadioQuiet(BasePointSource):
         ----------
         ImgMat: np.ndarray
             Two dimensional matrix, to describe the image
-        self.ps_catelog: pandas.core.frame.DataFrame
+        self.ps_catalog: pandas.core.frame.DataFrame
             Data of the point sources
         freq: float
             frequency
@@ -62,14 +40,17 @@ class RadioQuiet(BasePointSource):
         # Init
         npix = hp.nside2npix(self.nside)
         hpmap = np.zeros((npix,))
-        # Gen flux list
-        ps_flux_list = self.calc_flux(freq)
-        # Angle to pix
-        lat = (self.ps_catelog['Lat (deg)'] + 90) / 180 * np.pi
-        lon = self.ps_catelog['Lon (deg)'] / 180 * np.pi
-        pix = hp.ang2pix(self.nside, lat, lon)
-        # Gen hpmap
-        hpmap[pix] += ps_flux_list
+        # Gen Tb list
+        Tb_list = self.calc_Tb(freq)
+        #  Iteratively draw the ps
+        num_ps = self.ps_catalog.shape[0]
+        for i in range(num_ps):
+            # Angle to pix
+            lat = (self.ps_catalog['Lat (deg)'] + 90) / 180 * np.pi
+            lon = self.ps_catalog['Lon (deg)'] / 180 * np.pi
+            pix = hp.ang2pix(self.nside, lat, lon)
+            # Gen hpmap
+            hpmap[pix] += Tb_list[i]
 
         return hpmap
 
@@ -81,12 +62,66 @@ class RadioQuiet(BasePointSource):
         # Init
         num_freq = len(freq)
         npix = hp.nside2npix(self.nside)
-        hpmaps = np.zeros((npix,num_freq))
+        hpmaps = np.zeros((npix, num_freq))
 
-        # Gen ps_catelog
-        self.gen_catelog()
+        # Gen ps_catalog
+        self.gen_catalog()
         # get hpmaps
         for i in range(num_freq):
             hpmaps[:, i] = self.draw_single_ps(freq[i])
 
         return hpmaps
+
+    def calc_single_Tb(self, area, freq):
+        """
+        Calculate brightness temperatur of a single ps
+
+        Parameters
+        ------------
+        area: `~astropy.units.Quantity`
+              Area of the PS, e.g., `1.0*au.sr2`
+        freq: `~astropy.units.Quantity`
+              Frequency, e.g., `1.0*au.MHz`
+
+        Return
+        ------
+        Tb:`~astropy.units.Quantity`
+             Average brightness temperature, e.g., `1.0*au.K`
+        """
+        # Init
+        freq_ref = 151 * au.MHz
+        freq = freq * au.MHz
+        # Refer to Wang et al,'s work listed above.
+        I_151 = 10**(np.random.uniform(-4, -3)) * au.Jy
+        # Calc flux
+        flux = (freq / freq_ref)**(-0.7) * I_151
+        # Calc brightness temperature
+        Tb = convert.Fnu_to_Tb(flux, area, freq)
+
+        return Tb
+
+    def calc_Tb(self, freq):
+        """
+        Calculate the surface brightness  temperature of the point sources.
+
+        Parameters
+        ------------
+        area: `~astropy.units.Quantity`
+             Area of the PS, e.g., `1.0*au.sr`
+        freq: `~astropy.units.Quantity`
+             Frequency, e.g., `1.0*au.MHz`
+
+        Return
+        ------
+        Tb_list: list
+             Point sources brightness temperature
+        """
+        # Tb_list
+        num_ps = self.ps_catalog.shape[0]
+        Tb_list = np.zeros((num_ps,))
+        # Iteratively calculate Tb
+        for i in range(num_ps):
+            ps_area = self.ps_catalog['Area (sr)'][i] * au.sr
+            Tb_list[i] = self.calc_single_Tb(ps_area, freq).value
+
+        return Tb_list
