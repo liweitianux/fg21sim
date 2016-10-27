5b57997fe623289077e89b545c0ca50b3bc34265
Zhixian Ma <zxma_sjtu@qq.com>
2016-10-27 11:02:06 +0800

    Reajusted code style of configuration loading.

--- fg21sim/extragalactic/pointsources/radioquiet.py
+++ fg21sim/extragalactic/pointsources/radioquiet.py
@@ -21,35 +21,26 @@ class RadioQuiet(BasePointSource):
     def _set_configs(self):
         """Load the configs and set the corresponding class attributes"""
         super()._set_configs()
+        pscomp = "extragalactic/pointsources/radioquiet/"
         # point sources amount
-        self.num_ps = self.configs.getn(
-            "extragalactic/pointsources/radioquiet/numps")
+        self.num_ps = self.configs.getn(pscomp+"numps")
         # prefix
-        self.prefix = self.configs.getn(
-            "extragalactic/pointsources/radioquiet/prefix")
+        self.prefix = self.configs.getn(pscomp+"prefix")
         # redshift bin
-        z_type = self.configs.getn(
-            "extragalactic/pointsources/radioquiet/z_type")
+        z_type = self.configs.getn(pscomp+"z_type")
         if z_type == 'custom':
-            start = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/z_start")
-            stop = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/z_stop")
-            step = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/z_step")
+            start = self.configs.getn(pscomp+"z_start")
+            stop = self.configs.getn(pscomp+"z_stop")
+            step = self.configs.getn(pscomp+"z_step")
             self.zbin = np.arange(start, stop + step, step)
         else:
             self.zbin = np.arange(0.1, 10, 0.05)
         # luminosity bin
-        lumo_type = self.configs.getn(
-            "extragalactic/pointsources/radioquiet/lumo_type")
+        lumo_type = self.configs.getn(pscomp+"lumo_type")
         if lumo_type == 'custom':
-            start = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/lumo_start")
-            stop = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/lumo_stop")
-            step = self.configs.getn(
-                "extragalactic/pointsources/radioquiet/lumo_step")
+            start = self.configs.getn(pscomp+"lumo_start")
+            stop = self.configs.getn(pscomp+"lumo_stop")
+            step = self.configs.getn(pscomp+"lumo_step")
             self.lumobin = np.arange(start, stop + step, step)
         else:
             self.lumobin = np.arange(18.7, 25.7, 0.1)  # [W/Hz/sr]
