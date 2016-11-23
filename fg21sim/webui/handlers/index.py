58bc8b4bca4a0272ea78294d8aa413598eaf75c6
Aaron LI <aaronly.me@outlook.com>
2016-11-23 16:39:29 +0800

    webui: Add "from_localhost" to IndexHandler and products template

--- fg21sim/webui/handlers/index.py
+++ fg21sim/webui/handlers/index.py
@@ -11,6 +11,18 @@ from .base import BaseRequestHandler
 class IndexHandler(BaseRequestHandler):
     """
     Index page handler of the Web UI.
+
+    Attributes
+    ----------
+    from_localhost : bool
+        ``True`` if the request is from the localhost, otherwise ``False``.
     """
+    def initialize(self):
+        """Hook for subclass initialization.  Called for each request."""
+        if self.request.remote_ip == "127.0.0.1":
+            self.from_localhost = True
+        else:
+            self.from_localhost = False
+
     def get(self):
-        self.render("index.html")
+        self.render("index.html", from_localhost=self.from_localhost)
