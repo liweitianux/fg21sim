clusters/emission.py: Fix units/dimensions problem
NOTE:
Since we just substitute ``p`` (electron momentum) for
``γ`` (electron Lorentz factor), i.e.,
N = int_p n(p) dp = int_γ n(γ) dγ ,
therefore the coefficient ``sqrt(3) e^3 B / (m c^2)`` does NOT need
modification (e.g., multiply an extra ``m c``)!

Signed-off-by: Aaron LI <aly@aaronly.me>

fg21sim/extragalactic/clusters/emission.py
@@ -185,7 +185,8 @@ class SynchrotronEmission:
         # Integrate over energy ``gamma`` in logarithmic grid
         j_nu = integrate.simps(s1d*self.gamma, np.log(self.gamma))
 
-        coef = np.sqrt(3) * AC.e**3 * self.B / AC.c
+        B_gauss = self.B * 1e-6  # [uG] -> [G]
+        coef = np.sqrt(3) * AC.e**3 * B_gauss / AU.mec2
         j_nu *= coef
         return j_nu
 