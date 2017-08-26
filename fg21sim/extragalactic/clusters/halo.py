ec2b93030d0077d2598f523dab4f6df8ed2e09b4
Aaron LI <aly@aaronly.me>
2017-08-26 23:33:35 +0800

    clusters/halo.py: Update injection rate calculation
    The electrons are assumed to be injected throughout the whole cluster
    ICM/volume.

--- fg21sim/extragalactic/clusters/halo.py
+++ fg21sim/extragalactic/clusters/halo.py
@@ -229,27 +229,22 @@ class RadioHalo:
         life (e.g., ``age_obs`` here) is a fraction (``self.eta_e``)
         of the total thermal energy of the cluster.
 
-        Note that we assume that the relativistic electrons only permeate
-        the halo volume (i.e., of radius ``self.radius``) instead of the
-        whole cluster volume (of virial radius).
+        The electrons are assumed to be injected throughout the cluster
+        ICM/volume, i.e., do not restricted inside the halo volume.
 
         Qe(γ) = Ke * γ^(-s),
-        int[ Qe(γ) γ me c^2 ]dγ * t_cluster * V_halo =
-            eta_e * e_th * V_cluster
+        int[ Qe(γ) γ me c^2 ]dγ * t_cluster = eta_e * e_th
         =>
-        Ke = [(s-2) * eta_e * e_th * γ_min^(s-2) * (R_vir/R_halo)^3 /
-              me / c^2 / t_cluster]
+        Ke = [(s-2) * eta_e * e_th * γ_min^(s-2) / (me * c^2 * t_cluster)]
 
         References
         ----------
         Ref.[cassano2005],Eqs.(31,32,33)
         """
         s = self.injection_index
-        R_halo = self.radius  # [kpc]
-        R_vir = helper.radius_virial(self.M_obs, self.z_obs)  # [kpc]
         e_thermal = helper.density_energy_thermal(self.M_obs, self.z_obs)
         term1 = (s-2) * self.eta_e * e_thermal  # [erg cm^-3]
-        term2 = self.gamma_min**(s-2) * (R_vir/R_halo)**3
+        term2 = self.gamma_min**(s-2)
         term3 = AU.mec2 * self.age_obs  # [erg Gyr]
         Ke = term1 * term2 / term3  # [cm^-3 Gyr^-1]
         return Ke
