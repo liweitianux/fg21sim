9ed7dda1244b0c3a70cb98ff649957efcd3b8f74
Aaron LI <aly@aaronly.me>
2017-08-27 14:32:52 +0800

    Bump version to 0.6.0, prepare for a new release
    Since last release (0.4.2), too many things have changed ...
    so skip 0.5.x to emphasis the big update ...
    
    Also update the URL to the GitHub one.

--- fg21sim/__init__.py
+++ fg21sim/__init__.py
@@ -1,19 +1,19 @@
 """
-Realistic Foregrounds Simulation for EoR 21cm Signal Detection
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Realistic Foreground Simulation for EoR 21cm Signal Detection
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2016-2017 Weitian LI
 :license: MIT
 """
 
 __pkgname__ = "fg21sim"
-__version__ = "0.4.2"
+__version__ = "0.6.0"
 __author__ = "Weitian LI"
 __author_email__ = "weitian@aaronly.me"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2016-2017 Weitian LI"
-__url__ = "https://code.aaronly.me/fg21sim.git"
-__description__ = ("Realistic Foregrounds Simulation for "
+__url__ = "https://github.com/liweitianux/fg21sim"
+__description__ = ("Realistic Foreground Simulation for "
                    "EoR 21cm Signal Detection")
 
 
