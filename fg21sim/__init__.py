88d1d4151a093facf32a33b52d6b687686989666
Aaron LI <aly@aaronly.me>
2018-05-22 23:14:42 +0800

    Bump to version v0.7.1

--- fg21sim/__init__.py
+++ fg21sim/__init__.py
@@ -1,20 +1,12 @@
-"""
-Realistic Foreground Simulation for 21 cm Reionization Signal Detection
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-:copyright: (c) 2016-2018 Weitian LI
-:license: MIT
-"""
-
 __pkgname__ = "FG21sim"
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __date__ = "2018-01-16"
 __author__ = "Weitian LI"
 __author_email__ = "weitian@aaronly.me"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2016-2018 Weitian LI"
 __url__ = "https://github.com/liweitianux/fg21sim"
-__description__ = ("Realistic Foreground Simulation for "
+__description__ = ("Foreground Simulation for "
                    "21 cm Reionization Signal Detection")
 
 
